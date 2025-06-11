76e257fe3dc046573dae8025847f67dc0e9d7d5b
saad <jdiasaad86@gmail.com>
2025-06-11 16:41:40 +0100

    add the function ft_putchar to the ft_putstr fuction

--- ft_putstr/ft_putstr.c
+++ ft_putstr/ft_putstr.c
@@ -1,19 +1,26 @@
 #include <unistd.h>
 
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
 void ft_putstr(char *str)
 {
-    int i = 0;
+    int i;
 
-    if(!str)
+    i = 0;
+    if (!str)
         return;
-    while(str[i] != '\0'){
-        write(1, &str[i],1);
+
+    while (str[i] != '\0')
+    {
+        ft_putchar(str[i]);
         i++;
     }
 }
-
 int main(void)
 {
-    ft_putstr("hello");
+    ft_putstr("saad jdia");
 }
-
