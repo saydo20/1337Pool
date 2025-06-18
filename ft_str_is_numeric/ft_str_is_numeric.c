118ff124b521923171a1d080aa7ef2468e0417ef
saad <jdiasaad86@gmail.com>
2025-06-18 17:10:05 +0100

    complet the ft_str_is_numeric function

---
+++ ft_str_is_numeric/ft_str_is_numeric.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int ft_str_is_numeric(char *str){
+    int i = 0;
+    while(str[i] != '\0'){
+        if (!(str[i] >= '0' && str[i] <= '9'))
+            return 0;
+        i++;
+    }
+    return 1;
+}
+int main(void)
+{
+    char name[] = "saad";
+    printf("%d", ft_str_is_numeric(name));
+}
