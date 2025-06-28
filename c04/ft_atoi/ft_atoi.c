69f8d6fc2b4457c0f1a59d912284e8ec6e4a916b
saad <jdiasaad86@gmail.com>
2025-06-28 16:01:48 +0100

    edit the ft_atoi function

--- c04/ft_atoi/ft_atoi.c
+++ c04/ft_atoi/ft_atoi.c
@@ -3,37 +3,36 @@
 
 int ft_atoi(char *str)
 {
-
     int i = 0;
-    while (str[i])
-    {
-        if (str[i] != '\n' && str[i] != '\f' && str[i] != '\t' && str[i] != ' ' && str[i] != '\v' && str[i] != '\r')
-            break;
-        i++;
-    }
     int mn = 0;
-    while (str[i])
+    int result = 0;
+
+    while (str[i] == ' ' || str[i] == '\n' || str[i] == '\f' ||
+           str[i] == '\t' || str[i] == '\v' || str[i] == '\r')
+        i++;
+
+    while (str[i] == '+' || str[i] == '-')
     {
-        if (str[i] != '-' && str[i] != '+')
-            break;
-        if (str[i] = '-')
+        if (str[i] == '-')
             mn++;
         i++;
     }
 
-    int s1 = 0;
-    int j = 0;
-    while (str[i] && str[i] >= 49 && str[i] <= 57)
+    while (str[i] && str[i] >= '0' && str[i] <= '9')
     {
-        s1 = s1 * 10 + (str[i] - '0');
+        result = result * 10 + (str[i] - '0');
         i++;
     }
-    if(mn % 2 == 1)
-        s1 = -s1;
-    return s1;
+
+    if (mn % 2 == 1)
+        result = -result;
+
+    return result;
 }
+
 int main(void)
 {
     char s[] = "    ---+--+1234ab567";
-    printf("%d", ft_atoi(s));
+    printf("%d\n", ft_atoi(s));
+    return 0;
 }
